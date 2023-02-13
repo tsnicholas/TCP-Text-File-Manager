24f472e270c9de70285f6a2deb0e31a884da43d0
Tim Nicholas <tsnicholas@bsu.edu>
2023-02-13 10:12:01 -0500

    Refactor for different commands.

--- src/main/java/tcp_file_project/ClientTCP.java
+++ src/main/java/tcp_file_project/ClientTCP.java
@@ -1,40 +1,31 @@
 package tcp_file_project;
 
+import java.io.File;
 import java.io.IOException;
-import java.net.InetSocketAddress;
-import java.nio.ByteBuffer;
-import java.nio.channels.SocketChannel;
-import java.util.Scanner;
 
 public class ClientTCP {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) {
         if(args.length != 2) {
-            System.out.println("Syntax: ClientTCP <ServerIP> <ServerPort>");
+            System.out.println("Syntax: ClientTCP <Command> (<FileName>)");
             return;
         }
-        String serverIP = args[0];
-        int serverPort = Integer.parseInt(args[1]);
-        // Prompt the user for a message.
-        Scanner scanner = new Scanner(System.in);
-        System.out.print("Enter a message: ");
-        String msg = scanner.nextLine();
-        ByteBuffer buffer = ByteBuffer.wrap(msg.getBytes());
-        // create a SocketChannel by calling open()
-        SocketChannel sc = SocketChannel.open();
-        // Initiate a connection request to server.
-        // This request will be handled by the accept() on the server side.
-        sc.connect(new InetSocketAddress(serverIP, serverPort));
-        // Send message to the server side.
-        sc.write(buffer);
-        // Important: must shut down output when done sending.
-        sc.shutdownOutput();
-        // Read from the server to receive the response.
-        buffer.flip();
-        sc.read(buffer);
-        sc.close();
-        // Display the response.
-        buffer.flip();
-        byte[] response = buffer.array();
-        System.out.println(new String(response));
+        ClientTCP clientTCP = new ClientTCP();
+        clientTCP.performCommand(args[0], args[1]);
+    }
+
+    private void performCommand(String command, String fileName) {
+        try {
+            switch(command) {
+                // TODO: Add the rest of the commands here.
+                case "del" -> deleteFile(fileName);
+                default -> System.out.println("Not a valid command.");
+            }
+        } catch(IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    private void deleteFile(String fileName) throws IOException {
+        File file = new File(fileName);
     }
 }
