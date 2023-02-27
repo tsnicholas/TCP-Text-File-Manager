5577d7ff012c57fba2ef57f599527d11c7e24cfd
Tim Nicholas <tsnicholas@bsu.edu>
2023-02-27 10:45:14 -0500

    Adds upload command and refactors code to be cleaner.

--- src/main/java/tcp_file_project/ClientTCP.java
+++ src/main/java/tcp_file_project/ClientTCP.java
@@ -1,6 +1,6 @@
 package tcp_file_project;
 
-import java.io.IOException;
+import java.io.*;
 import java.net.InetSocketAddress;
 import java.nio.ByteBuffer;
 import java.nio.channels.SocketChannel;
@@ -10,7 +10,9 @@ public class ClientTCP {
     private static final String RENAME = "r";
     private static final String DELETE = "d";
     private static final String LIST = "l";
+    private static final String UPLOAD = "u";
     private static final String SEPARATOR = "%";
+    private static final int MAX_TRANSFER_SIZE = 400;
     private static SocketChannel sc;
 
     public static void main(String[] args) {
@@ -40,6 +42,7 @@ public class ClientTCP {
             case DELETE -> deleteFile();
             case RENAME -> renameFile();
             case LIST -> listFile();
+            case UPLOAD -> uploadFile();
             default -> System.out.println("Not a valid command.");
         }
     }
@@ -48,23 +51,23 @@ public class ClientTCP {
         String output = DELETE + promptUser("Enter file name: ");
         sc.write(ByteBuffer.wrap(output.getBytes()));
         sc.shutdownOutput();
-        ByteBuffer buffer = getServerResponse();
-        printResponse(buffer);
+        printResponse(getServerResponse());
+        sc.close();
     }
 
     private void renameFile() throws IOException {
         String output = RENAME + promptUser("Enter File name: ") + SEPARATOR + promptUser("Rename file to: ");
         sc.write(ByteBuffer.wrap(output.getBytes()));
         sc.shutdownOutput();
-        ByteBuffer buffer = getServerResponse();
-        printResponse(buffer);
+        printResponse(getServerResponse());
+        sc.close();
     }
 
     private void listFile() throws IOException {
         sc.write(ByteBuffer.wrap(LIST.getBytes()));
         sc.shutdownOutput();
-        ByteBuffer buffer = getServerResponse();
-        printFileList(buffer);
+        System.out.println(getServerResponse());
+        sc.close();
     }
 
     private String promptUser(String prompt) {
@@ -73,27 +76,51 @@ public class ClientTCP {
         return scanner.nextLine();
     }
 
-    private ByteBuffer getServerResponse() throws IOException {
-        ByteBuffer buffer = ByteBuffer.allocate(400);
+    private String getServerResponse() throws IOException {
+        ByteBuffer buffer = ByteBuffer.allocate(MAX_TRANSFER_SIZE);
         sc.read(buffer);
-        sc.close();
-        return buffer;
+        buffer.flip();
+        // Replaces null characters before returning.
+        return new String(buffer.array()).replace("\0", "");
     }
 
-    private void printResponse(ByteBuffer buffer) {
-        buffer.flip();
-        byte[] response = buffer.array();
-        char responseCode = new String(response).charAt(0);
+    private void printResponse(String responseCode) {
         switch(responseCode) {
-            case 'S' -> System.out.println("Operation Successful.");
-            case 'F' -> System.out.println("Operation Failed.");
+            case "S" -> System.out.println("Operation Successful.");
+            case "F" -> System.out.println("Operation Failed.");
             default -> System.out.println("Unknown response from server.");
         }
     }
 
-    private void printFileList(ByteBuffer buffer) {
-        buffer.flip();
-        byte[] list = buffer.array();
-        System.out.println(new String(list).replace("\0", ""));
+    private void uploadFile() throws IOException {
+        String filePath = promptUser("Enter the absolute file path: ");
+        File file = new File(filePath);
+        if(!file.exists()) {
+            System.out.println("This file doesn't exist.");
+        } else {
+            writeFileToServer(file);
+        }
+        sc.close();
+    }
+
+    private void writeFileToServer(File file) throws IOException {
+        for(int i = 0; i < file.length(); i += MAX_TRANSFER_SIZE) {
+            String uploadData = UPLOAD + file.getName() + SEPARATOR + readFromFile(file, i);
+            sc.write(ByteBuffer.wrap(uploadData.getBytes()));
+            String responseCode = getServerResponse();
+            printResponse(responseCode);
+            // If for some reason the server fails during the process, terminate the loop.
+            if(responseCode.equals("F")) {
+                break;
+            }
+        }
+    }
+
+    private String readFromFile(File file, int i) throws IOException {
+        char[] readData = new char[MAX_TRANSFER_SIZE];
+        BufferedReader bufferedReader = new BufferedReader(new FileReader(file));
+        System.out.println("Uploading " +
+                bufferedReader.read(readData, i, i + MAX_TRANSFER_SIZE) + " characters...");
+        return new String(readData);
     }
 }
